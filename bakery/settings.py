aef8675c93d4a9a4cd7b6f06cc893fa3fe40796d
User <User@0371261-PPO.local.ukzn.ac.za>
2021-04-22 18:45:43 +0200

    'changes'

--- bakery/settings.py
+++ bakery/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'bakery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bakery',
-        'USER': 'postgres',
-        'PASSWORD': 'nexus',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
